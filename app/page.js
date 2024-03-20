acc6365790e8d9aeee27e1b331d878d986becd05
anto__dev <antonidwm@gmail.com>
2024-03-20 09:12:22 +0100

    ADD UI box intro component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,26 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import IntroBox from "./components/BoxIntro/index";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      Home
-    </main>
+    <>
+      <main className={styles.main}>Home</main>
+      <IntroBox
+        title1={"Bien le bonjour "}
+        txt1={
+          <>
+            Bienvenus dans ton gestionnaire de carte. Tu vas avoir un aperçu en
+            un rien de temps de ta collection.{" "}
+          </>
+        }
+        link1={<>Ajouter des cartes</>}
+        link2={<>Voir ta collection</>}
+        srcImg={
+          "https://images.unsplash.com/photo-1668720028726-c4997f31f216?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+        src1={"#"}
+        src2={"#"}
+      />
+    </>
   );
 }
