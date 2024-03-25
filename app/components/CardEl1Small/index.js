5e323819dc2b11bc17ff7bf727804b5b5a2ecbcc
anto__dev <antonidwm@gmail.com>
2024-03-25 02:34:45 +0100

    ADD function to number card inventaire

--- app/components/CardEl1Small/index.js
+++ app/components/CardEl1Small/index.js
@@ -1,58 +1,114 @@
-import React from 'react'
-import CardBoxTitle1 from '../atom/CardBoxTitle1'
-import './style.scss'
-import BoxSelectorInventaire from '../atom/BoxInventaireSelector'
+"use client";
 
-const CardElSmall = () => {
+import React, { useEffect, useState } from "react";
+import CardBoxTitle1 from "../atom/CardBoxTitle1";
+import "./style.scss";
+import BoxSelectorInventaire from "../atom/BoxInventaireSelector";
+
+const CardElSmall = ({
+  titleCard,
+  typeCard,
+  frameType,
+  numero,
+  level,
+  prixMkt,
+  nbCard,
+}) => {
+  const [nbCardCol, setNbCardCol] = useState(0);
+  const [idCard, setIdCard] = useState("");
+
+
+
+  ///function create id for the card
+var check22 = false;
+
+  if (idCard == "") {
+    let caracteresPossibles =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    function genererNombreAleatoireUnique(min, max) {
+      let nombreAleatoire;
+      do {
+        nombreAleatoire = Math.floor(Math.random() * (max - min + 1)) + min;
+      } while (nombreAleatoireDejaUtilise[nombreAleatoire]);
+      nombreAleatoireDejaUtilise[nombreAleatoire] = true;
+      return nombreAleatoire;
+    }
+    let idCardAl = "";
+    const nombreAleatoireDejaUtilise = new Array(
+      caracteresPossibles.length
+    ).fill(false);
+    for (let i = 0; i < 12; i++) {
+      const indiceAleatoire = genererNombreAleatoireUnique(
+        0,
+        caracteresPossibles.length - 1
+      );
+      idCardAl += caracteresPossibles[indiceAleatoire];
+    }
+
+    setIdCard(idCardAl);
+
+  }else{
+
+  }
+
+  ///
+
+  /// gestion for the base "data local.storage nb card"
+  const dataBoxInventaire = (data) => {
+    setNbCardCol(data);
+  };
+  ///
+
   return (
-   <>
-   
-   <div className="card">
-<CardBoxTitle1 title1={"Dragon blanc aux yeux bleus"} />
-< div className="card__containerCentral">
-    <div className="card__boxImg"><img src="https://images.ygoprodeck.com/images/cards_small/6983839.jpg" alt="Image of a yugi oh card " className="card__img" /></div>
-    <div className="card__boxInfos">
-        <h3 className="sect__title--3 card__title3">
-        Dragon blanc aux yeux bleus
-        </h3>
-        <div className="card__groupeInfos">
-            <p className="sect__txt sect__txt--light">
-                dragon
-            </p>
-            <p className="sect__txt sect__txt--light">
-               type
-            </p>
-            
+    <>
+      <div className="card" key={"card1"} id={idCard}>
+        <CardBoxTitle1 title1={titleCard} />
+        <div className="card__containerCentral">
+          <div className="card__boxImg">
+            <img
+              src="https://images.ygoprodeck.com/images/cards_small/6983839.jpg"
+              alt="Image of a yugi oh card "
+              className="card__img"
+            />
+          </div>
+          <div className="card__boxInfos">
+            <h3 className="sect__title--3 card__title3">
+              {titleCard} {idCard}
+            </h3>
+            <div className="card__groupeInfos">
+              <p className="sect__txt sect__txt--light">{typeCard}</p>
+              <p className="sect__txt sect__txt--light">{frameType}</p>
+            </div>
+            <div className="card__groupeInfos">
+              <p className="sect__txt sect__txt--light">{numero}</p>
+              <p className="sect__txt sect__txt--light">
+                Niveaux : <span className="sect__txt--bold"> {level} </span>
+              </p>
+            </div>
+            <div className="card__groupeInfos">
+              <p className="sect__txt sect__txt--light">
+                CMKT : <span className="sect__txt--bold">{prixMkt}</span>
+              </p>
+              <p className="sect__txt sect__txt--light">
+                Total :{" "}
+                <span className="sect__txt--bold">{prixMkt * nbCardCol}</span>
+              </p>
+              <p className="sect__txt sect__txt--light">
+                En collection :{" "}
+                <span className="sect__txt--bold">{nbCardCol}</span>
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="card__groupeInfos">
-            <p className="sect__txt sect__txt--light">
-                dragon
-            </p>
-            <p className="sect__txt sect__txt--light">
-                Niveaux : <span className='sect__txt--bold'>8</span>
-            </p>
-            
-        </div>
-        <div className="card__groupeInfos">
-        
-            <p className="sect__txt sect__txt--light">
-                CMKT : <span className='sect__txt--bold'>8</span>
-            </p>
-            <p className="sect__txt sect__txt--light">
-                Total : <span className='sect__txt--bold'>8</span>
-            </p>
-            <p className="sect__txt sect__txt--light">
-                En collection : <span className='sect__txt--bold'>8</span>
-            </p>
-        </div>
-    </div>
-</div>
 
-<BoxSelectorInventaire show1={true} />
-   </div>
+        <BoxSelectorInventaire
+          show1={true}
+          idCard={idCard}
+          UpNb={dataBoxInventaire}
+        />
+      </div>
+    </>
+  );
+};
 
-   </>
-  )
-}
-
-export default CardElSmall
+export default CardElSmall;
--- app/components/atom/BoxInventaireSelector/index.js
+++ app/components/atom/BoxInventaireSelector/index.js
@@ -1,7 +1,39 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
 import "./styles.scss";
 
-const BoxSelectorInventaire = ({show1}) => {
+const BoxSelectorInventaire = ({show1, idCard, UpNb}) => {
+
+  const [nbInCol, setNbInCol] = useState(0)
+
+  function addToCol(){
+    //localStorage.setItem(idCardAl+"nb", 0);
+    let nbCardOld = +localStorage.getItem(idCard+"nb");
+    let newNb = nbCardOld + 1 ;
+    localStorage.setItem(idCard+"nb", newNb);
+    setNbInCol(newNb);
+    UpNb(newNb);
+  
+
+  }
+
+  function minToCol(){
+    let nbCardOld = +localStorage.getItem(idCard+"nb");
+    if(nbCardOld === 0){
+      let newNb = 0;
+      localStorage.setItem(idCard+"nb", newNb);
+      setNbInCol(newNb);
+      UpNb(newNb);
+    }else { 
+      let newNb = nbCardOld - 1 ;
+      localStorage.setItem(idCard+"nb", newNb);
+      setNbInCol(newNb);
+      UpNb(newNb);
+    }
+    
+    
+  }
   return (
     <>
       {/* <div className="card__selectorNbInv">
@@ -41,30 +73,30 @@ const BoxSelectorInventaire = ({show1}) => {
       {show1? <><div className="card__selectorNbInv">
         <ul className="selectorNbInv__list">
           <li className="selectorNbInv__el">
-            <button className="selectorNbInv__btn selectorNbInv__btn--min">
+            <button className="selectorNbInv__btn selectorNbInv__btn--min" onClick={minToCol}>
               -
             </button>
           </li>
           <li className="selectorNbInv__el">
-            <p className="selectorNbInv__txt">0</p>
+            <p className="selectorNbInv__txt">{nbInCol }</p>
           </li>
           <li className="selectorNbInv__el">
-            <button className="selectorNbInv__btn">+</button>
+            <button className="selectorNbInv__btn" onClick={addToCol}>+</button>
           </li>
         </ul>
       </div></>:<><div className="card__selectorNbInv card__selectorNbInv--small">
           <h3 className="sect__title sect__title--2  sect__title--slice selectorNbInv__title">
             Inventaire
           </h3>
-          <p className="selectorNbInv__txt selectorNbInv__txt--center">0</p>
+          <p className="selectorNbInv__txt selectorNbInv__txt--center">{nbInCol }</p>
           <ul className="selectorNbInv__listBtn">
             <li className="selectorNbInv__elBtn">
-              <button className="selectorNbInv__btn selectorNbInv__btn--small">
+              <button className="selectorNbInv__btn selectorNbInv__btn--small"  onClick={minToCol}>
                 -
               </button>
             </li>
             <li className="selectorNbInv__elBtn">
-              <button className="selectorNbInv__btn selectorNbInv__btn--small">
+              <button className="selectorNbInv__btn selectorNbInv__btn--small" onClick={addToCol}>
                 +
               </button>
             </li>
